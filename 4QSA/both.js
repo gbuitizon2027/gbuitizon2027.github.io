a476ca86d243a910e1dc3fe0885b5746f6d84e15
gbuitizon2027 <143575321+gbuitizon2027@users.noreply.github.com>
2025-07-05 19:44:23 +0800

    Delete 4QSA directory

--- 4QSA/both.js
+++
@@ -1,8 +0,0 @@
-let shoppingList = localStorage.getItem("shoppingList") || [];
-
-if (shoppingList === null) {
-  shoppingList = [];
-} else {
-  // Parse the stored string back to an array
-  shoppingList = JSON.parse(shoppingList);
-}
